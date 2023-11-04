62f2a534bce29a3f4a72856038d0fc9b1c796016
Lucas Fernandes <142260544+LepoFernandes@users.noreply.github.com>
2023-11-03 23:35:13 -0300

    Create script.js

---
+++ ex01/script.js
@@ -0,0 +1,8 @@
+console.info("Olá mundo! Bem vindo à nave mãe elemental.")
+console.log("Olá mundo! Bem vindo à nave mãe elemental.")
+console.warn("Olá mundo! Bem vindo à nave mãe elemental.")
+console.error("Erro ao entrar na nave! Tente novamente")
+alert("Bem vindo a nave elemental!") 
+confirm("Tudo pronto para partida? Aguardando confirmação.")
+prompt("Qual seu nome, piloto?")
+//EXPLICAÇÃO DE CADA UM NO CADERNO
