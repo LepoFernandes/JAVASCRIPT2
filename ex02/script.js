719b6b6d7886a5bc7a89e90d8e382bc9c621cfcb
Lucas Fernandes <142260544+LepoFernandes@users.noreply.github.com>
2023-11-06 17:02:14 -0300

    exnew

---
+++ ex02/script.js
@@ -0,0 +1,10 @@
+onload = function(){
+    let nome = prompt("Seja bem-vindo(a), por favor digite seu nome abaixo.")
+    let idade = prompt(`Olá ${nome} , qual sua idade?`)
+    confirm(`Confirme a sua idade por favor, você tem ${idade} anos?`)
+    alert(`Seja bem-vindo(a) ${nome}, sua idade é ${idade}. `)
+}
+
+
+
+
---
+++ ex03/script.js
@@ -0,0 +1,12 @@
+
+
+    let nomemaisvelha = prompt("Olá, qual seu nome?")
+    let idademaisvelha = prompt(`Olá ${nomemaisvelha}, qual sua idade?`)
+
+    let nomemaisnova = prompt("Olá, qual seu nome?")
+    let idademaisnova = prompt(`Olá ${nomemaisnova}, qual sua idade?`)
+
+    let resultado = idademaisvelha - idademaisnova
+    
+    alert(`A diferença de idade entre ${nomemaisvelha} e ${nomemaisnova} é de ${resultado} anos`)
+
